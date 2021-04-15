cb7063cc198491b3b3986733188a998e51d35268
Eric Horodyski <horodyski@ionic.io>
2021-04-15 11:30:07 -0400

    Add platforms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { InAppBrowser } from '@ionic-enterprise/inappbrowser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    InAppBrowser,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@ionic-enterprise/inappbrowser/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(private iab: InAppBrowser, private platform: Platform) {}
 
-  constructor() {}
-
+  async ngOnInit() {
+    await this.platform.ready();
+    const browser = this.iab.create(
+      'https://www.parsippanysbestpizza.com/',
+      '_blank'
+    );
+    browser.show();
+  }
 }
